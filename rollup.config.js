5498f823a6287073864d56e7efc5c9c0abf77666
Daniel Barclay <barclaysd@me.com>
2021-01-01 23:32:40 +0000

    Add rollup support and rollup.config.js

---
+++ rollup.config.js
@@ -0,0 +1,40 @@
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import typescript from '@rollup/plugin-typescript';
+import css from 'rollup-plugin-css-only';
+import fs from 'fs';
+import path from 'path';
+
+const production = !process.env.ROLLUP_WATCH ?? false;
+
+export default fs
+  .readdirSync(path.join(__dirname, 'webviews', 'pages'))
+  .map((input) => {
+    const name = input.split('.')[0];
+    return {
+      input: 'webviews/pages/' + input,
+      output: [
+        {
+          file: 'out/compiled/' + name + '.js',
+          name: 'app',
+          sourcemap: 'true',
+          format: 'iife',
+        },
+      ],
+      plugins: [
+        peerDepsExternal(),
+        resolve({
+          browser: true,
+          dedupe: ['react'],
+        }),
+        commonjs(),
+        typescript({
+          tsconfig: 'webviews/tsconfig.json',
+          sourceMap: !production,
+          inlineSources: !production,
+        }),
+        css({ output: 'bundle.css' }),
+      ],
+    };
+  });
