af301c3061caf8423cd38c660a1205e750df2320
Daniel Barclay <barclaysd@me.com>
2021-01-02 17:33:41 +0000

    Add placeholder logic to sidebar

--- webviews/components/Sidebar.tsx
+++ webviews/components/Sidebar.tsx
@@ -1,3 +1,26 @@
 import './sidebar.css';
+import { useState } from 'react';
 
-export const Sidebar = () => <h1>PR Finder</h1>;
+export const Sidebar = () => {
+  const [input, setInput] = useState('');
+  return (
+    <>
+      <h1>PR Finder</h1>
+      <input type="text" value={input} onChange={(event) => setInput(event.target.value)}/>
+      <div>{input}</div>
+      <button onClick={() => setInput('')}>Reset</button>
+      <button onClick={() => {
+        tsvscode.postMessage({
+          type: 'onInfo',
+          value: 'info message',
+        });
+      }}>Info</button>
+      <button onClick={() => {
+        tsvscode.postMessage({
+          type: 'onError',
+          value: 'info message',
+        });
+      }}>Error</button>
+    </>
+  );
+};
