a130d3d3b0fd46418332fb7d88e49c66b8568ace
Daniel Barclay <barclaysd@me.com>
2021-01-06 13:27:01 +0000

    Start polka server to listen for github oauth redirect

--- src/authenticate.ts
+++ src/authenticate.ts
@@ -1,5 +1,31 @@
 import * as vscode from 'vscode';
+import * as polka from 'polka';
 import { api } from '../package.json';
+
+const PORT = 54321;
+
 export const authenticate = () => {
-  vscode.commands.executeCommand('vscode.open', vscode.Uri.parse(`${api.production}/auth/github`))
-}
+  const app = polka();
+  app.get('/auth/:userData', (req, res) => {
+    const userData: string | undefined = req.params.userData;
+    if (!userData) {
+      res.end('<h1>Something went wrong</h1>');
+    }
+    const uriDecodedUserData = decodeURIComponent(userData);
+    const data = Buffer.from(uriDecodedUserData, 'base64').toString('utf-8');
+    console.log(data);
+    res.end('<h1>Successfully authenticated. You can close this now</h1>');
+  });
+  app.listen(PORT, (error: Error | undefined) => {
+    if (error) {
+      vscode.window.showErrorMessage(
+        'Error occurred in authenticating for PR Finder',
+      );
+    }
+    console.log(`Launched server on port ${PORT} 🚀`);
+    vscode.commands.executeCommand(
+      'vscode.open',
+      vscode.Uri.parse(`${api.production}/auth/github`),
+    );
+  });
+};
