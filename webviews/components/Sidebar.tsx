dfed7e922b20075386bf9b3571047344ccbe3ae2
Daniel Barclay <barclaysd@me.com>
2021-01-07 13:49:10 +0000

    Pass accessToken between extension and Sidebar

--- webviews/components/Sidebar.tsx
+++ webviews/components/Sidebar.tsx
@@ -1,20 +1,28 @@
 import './sidebar.css';
 import { useEffect } from 'react';
-import { vsCodeData } from '../types';
+import { Message, vsCodeData } from '../../globals/types';
 
 export const Sidebar = () => {
+  let accessToken: string | undefined;
 
   useEffect(() => {
     window.addEventListener('message', (event) => {
       const message: vsCodeData = event.data;
       switch (message.type) {
-        case 'add-repo':
+        case Message.addRepo:
           console.log(message);
+          break;
+        case Message.getToken:
+          accessToken = message.value;
+          console.log(accessToken);
       }
     });
+
+    tsVscode.postMessage({
+      type: Message.getToken,
+      value: '',
+    });
   }, []);
-
-
 
   return (
     <>
