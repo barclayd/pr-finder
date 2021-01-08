310d04c1c6df84ddb0bd4eb25d5438a990b0232d
Daniel Barclay <barclaysd@me.com>
2021-01-08 15:28:01 +0000

    Improve table click event handling and styling

--- webviews/components/Table.tsx
+++ webviews/components/Table.tsx
@@ -1,23 +1,35 @@
+import './table.css';
+
 interface TableProps<T> {
   records: T[];
   onRecordClick: (record: T) => void;
 }
 
-export const Table = <T extends object>({ records, onRecordClick }: TableProps<T>) => {
+export const Table = <T extends object>({
+  records,
+  onRecordClick,
+}: TableProps<T>) => {
   return (
     <table className="content-table">
       <thead>
         <tr>
           {Object.keys(records[0]).map((column, index) => (
-            <th key={column+index}>{column}</th>
+            <th key={column + index}>{column !== 'track' ? column : '✓'}</th>
           ))}
         </tr>
       </thead>
       <tbody>
         {records.map((record, index) => (
-          <tr key={record.toString()+index} onClick={() => onRecordClick(record)}>
+          <tr
+            key={record.toString() + index}
+          >
             {Object.values(record).map((data, index) => (
-              <td key={record.toString()+index}>{data}</td>
+              <td key={record.toString() + index} onClick={() => {
+                if (index === Object.values(record).length-1) {
+                  return;
+                }
+                onRecordClick(record)
+              }}>{data}</td>
             ))}
           </tr>
         ))}
