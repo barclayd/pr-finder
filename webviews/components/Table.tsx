f0fc4e0880e58ca619b292616bcb906edcc9eebf
Daniel Barclay <barclaysd@me.com>
2021-01-08 23:42:12 +0000

    Table component supports custom <TableHead />, improved styling

--- webviews/components/Table.tsx
+++ webviews/components/Table.tsx
@@ -1,35 +1,59 @@
 import './table.css';
+import { FC } from 'react';
 
 interface TableProps<T> {
   records: T[];
+  tableName?: string;
+  checkbox?: boolean;
   onRecordClick: (record: T) => void;
 }
 
 export const Table = <T extends object>({
   records,
   onRecordClick,
+  tableName,
+  checkbox,
 }: TableProps<T>) => {
+  const TableHead: FC = () => {
+    if (!tableName) {
+      return (
+        <>
+          {Object.keys(records[0]).map((column, index) => (
+            <th key={column + index}>{column !== 'track' ? column : '✓'}</th>
+          ))}
+        </>
+      );
+    }
+    return (
+      <>
+        <th>{tableName}</th>
+        <th>✓</th>
+      </>
+    );
+  };
+
   return (
     <table className="content-table">
       <thead>
         <tr>
-          {Object.keys(records[0]).map((column, index) => (
-            <th key={column + index}>{column !== 'track' ? column : '✓'}</th>
-          ))}
+          <TableHead />
         </tr>
       </thead>
-      <tbody>
+      <tbody className={checkbox ? 'checkbox': undefined}>
         {records.map((record, index) => (
-          <tr
-            key={record.toString() + index}
-          >
+          <tr key={record.toString() + index}>
             {Object.values(record).map((data, index) => (
-              <td key={record.toString() + index} onClick={() => {
-                if (index === Object.values(record).length-1) {
-                  return;
-                }
-                onRecordClick(record)
-              }}>{data}</td>
+              <td
+                key={record.toString() + index}
+                onClick={() => {
+                  if (index === Object.values(record).length - 1) {
+                    return;
+                  }
+                  onRecordClick(record);
+                }}
+              >
+                {data}
+              </td>
             ))}
           </tr>
         ))}
