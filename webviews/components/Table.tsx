9e1453a19a11729733959ee0d798f6a3c5b7fa4d
Daniel Barclay <barclaysd@me.com>
2021-01-10 00:49:55 +0000

    Add Caret SVG. Improve layout of <Table /> for both single column and multi column

--- webviews/components/Table.tsx
+++ webviews/components/Table.tsx
@@ -8,6 +8,24 @@ interface TableProps<T> {
   onRecordClick: (record: T) => void;
 }
 
+const Caret: FC = () => (
+  <svg
+    aria-hidden="true"
+    focusable="false"
+    data-prefix="fas"
+    data-icon="caret-right"
+    className="svg-inline--fa fa-caret-right fa-w-6"
+    role="img"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 192 512"
+  >
+    <path
+      fill="currentColor"
+      d="M0 384.662V127.338c0-17.818 21.543-26.741 34.142-14.142l128.662 128.662c7.81 7.81 7.81 20.474 0 28.284L34.142 398.804C21.543 411.404 0 402.48 0 384.662z"
+    ></path>
+  </svg>
+);
+
 export const Table = <T extends object>({
   records,
   onRecordClick,
@@ -25,21 +43,26 @@ export const Table = <T extends object>({
       );
     }
     return (
-      <>
-        <th>{tableName}</th>
-        <th>✓</th>
-      </>
+      <th>
+        <Caret />
+        {tableName}
+      </th>
     );
   };
 
   return (
-    <table className="content-table">
+    <table
+      className={[
+        'content-table',
+        Object.keys(records[0]).length === 1 ? 'single-column' : 'multi-column',
+      ].join(' ')}
+    >
       <thead>
         <tr>
           <TableHead />
         </tr>
       </thead>
-      <tbody className={checkbox ? 'checkbox': undefined}>
+      <tbody className={checkbox ? 'checkbox' : undefined}>
         {records.map((record, index) => (
           <tr key={record.toString() + index}>
             {Object.values(record).map((data, index) => (
