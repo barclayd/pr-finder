a045168c8acf661343df75af5d0c4ce29de74431
Daniel Barclay <barclaysd@me.com>
2021-03-21 12:38:53 +0000

    Simplify logic in SearchReposTab

--- webviews/components/tabs/SearchReposTab.tsx
+++ webviews/components/tabs/SearchReposTab.tsx
@@ -1,15 +1,7 @@
 import { useCombobox } from 'downshift';
 import type { DebouncedFunc } from 'lodash';
 import debounce from 'lodash/debounce';
-import {
-  Dispatch,
-  FC,
-  SetStateAction,
-  useEffect,
-  useRef,
-  useState,
-} from 'react';
-import { GithubUserOrganisation } from '../../../src/types';
+import { Dispatch, FC, SetStateAction, useRef, useState } from 'react';
 import { useAsyncEffect } from '../../hooks/useAsyncEffect';
 import { NetworkService } from '../../services/NetworkService';
 import {
@@ -17,8 +9,8 @@ import {
   GithubSearchRepo,
   GithubSearchResult,
 } from '../../types';
-import { CloseIcon } from '../icons/CloseIcon';
 import { SearchIcon } from '../icons/SearchIcon';
+import { SearchOrgs } from '../SearchOrgs';
 
 interface SearchReposTab {
   trackedRepos: GithubSearchRepo[];
@@ -50,8 +42,6 @@ const findRepoByName = (
 const searchURL = (searchURI: string, repoOwner: string) =>
   `https://api.github.com/search/repositories?q=${searchURI}%20in:name,description+org:${repoOwner}&per_page=100`;
 
-const GITHUB_USER_ORGANISATIONS = 'https://api.github.com/user/orgs';
-
 export const SearchReposTab = (props: SearchReposTab): AccordionItem => ({
   name: 'Search Repos',
   isEnabled: true,
@@ -66,17 +56,10 @@ const SearchRepos: FC<SearchReposProps> = ({
   networkService,
   accessToken,
 }) => {
-  const [allUserOrgs, setAllUserOrgs] = useState<GithubUserOrganisation[]>([]);
-  const [searchOrgRepo, setSearchOrgRepo] = useState(false);
-  const [searchableUserOrgs, setSearchableUserOrgs] = useState<
-    GithubUserOrganisation[]
-  >([]);
   const [selectedOrganisation, setSelectedOrganisation] = useState<
     string | undefined
   >(undefined);
   const [filteredRepos, setFilteredRepos] = useState<GithubSearchRepo[]>([]);
-
-  const [showRestrictionPrompt, setShowRestrictionPrompt] = useState(false);
 
   const [userInput, setUserInput] = useState<string | undefined>('');
   const debounceUserInput = useRef<LodashDebounceFunc | null>(null);
@@ -127,59 +110,8 @@ const SearchRepos: FC<SearchReposProps> = ({
     );
   };
 
-  useEffect(() => {
-    if (!searchOrgRepo) {
-      clearDropdownOnOrgChange();
-      setSelectedOrganisation(undefined);
-      return;
-    }
-    if (searchableUserOrgs.length === 0) {
-      return;
-    }
-    clearDropdownOnOrgChange();
-    setSelectedOrganisation(searchableUserOrgs[0].login);
-  }, [searchOrgRepo]);
-
-  useAsyncEffect(async () => {
-    const { data: allUserOrgs } =
-      (await networkService.get<GithubUserOrganisation[]>(
-        ALL_GITHUB_USER_ORGANISATIONS_URL,
-      )) ?? [];
-    setAllUserOrgs(allUserOrgs ?? []);
-    if (searchableUserOrgs.length !== allUserOrgs?.length) {
-      setShowRestrictionPrompt(true);
-    }
-  }, []);
-
-  useAsyncEffect(async () => {
-    const { data: userGithubOrganisations } = await networkService.get<
-      GithubUserOrganisation[]
-    >(GITHUB_USER_ORGANISATIONS);
-    if (!userGithubOrganisations) {
-      return;
-    }
-    setSearchableUserOrgs(userGithubOrganisations);
-  }, [accessToken]);
-
-  const missingOrgWarningTitle =
-    allUserOrgs.length > 0 && searchableUserOrgs.length === 0
-      ? 'No orgs available to search'
-      : "Can't find the orgs you were looking for?";
-
-  const ALL_GITHUB_USER_ORGANISATIONS_URL = `https://api.github.com/users/${username}/orgs`;
-
   const isNoResults =
     filteredRepos.length === 1 && filteredRepos[0].name === disabledGithubRepo;
-
-  const hideShowOrganisationRestrictionPrompt = () => {
-    setShowRestrictionPrompt(false);
-  };
-
-  const clearDropdownOnOrgChange = () => {
-    setFilteredRepos([]);
-    closeMenu();
-    setInputValue('');
-  };
 
   const openDropdown = () => {
     if (inputValue.trim().length === 0) {
@@ -239,47 +171,18 @@ const SearchRepos: FC<SearchReposProps> = ({
   });
 
   const showFoundResults = isOpen && !isNoResults;
-  const isSearchableOrgs = searchOrgRepo && searchableUserOrgs.length > 0;
 
   return (
     <>
-      <div className="organisation-checkbox">
-        <input
-          className="select-checkbox"
-          type="checkbox"
-          id="select-organisation"
-          onChange={() => setSearchOrgRepo(!searchOrgRepo)}
-        />
-        <label htmlFor="select-organisation">Search org repos</label>
-      </div>
-      {searchOrgRepo && showRestrictionPrompt && (
-        <div className="all-organisations-prompt">
-          <div className="organisation-prompt-title">
-            <span>{missingOrgWarningTitle}</span>
-            <CloseIcon onClick={hideShowOrganisationRestrictionPrompt} />
-          </div>
-          <span className="organisation-prompt">
-            Request permission from the org owners to allow PR Finder to view
-            them
-          </span>
-        </div>
-      )}
-      {isSearchableOrgs && (
-        <div className="user-organisations">
-          <label htmlFor="organisations">From: </label>
-          <select
-            id="organisations"
-            defaultValue={selectedOrganisation}
-            onChange={({ target: { value } }) => setSelectedOrganisation(value)}
-          >
-            {searchableUserOrgs.map((organisation) => (
-              <option value={organisation.login} key={organisation.login}>
-                {organisation.login}
-              </option>
-            ))}
-          </select>
-        </div>
-      )}
+      <SearchOrgs
+        accessToken={accessToken}
+        username={username}
+        networkService={networkService}
+        closeMenu={closeMenu}
+        setFilteredRepos={setFilteredRepos}
+        setSelectedOrganisation={setSelectedOrganisation}
+        setInputValue={setInputValue}
+      />
       <div className="input-wrapper" {...getComboboxProps()}>
         <button
           type="button"
